af738cca468849f30b2130ba842b116851f1c365
tmorrisiii86 <61118010+tmorrisiii86@users.noreply.github.com>
2020-03-22 13:01:24 -0500

    Add files via upload

---
+++ Lab10_4/MediaPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_4
+{
+    class MediaPlayer
+    {
+        public void PlayMedia(IAudioPlayer media)
+        {
+            media.Play();
+        }
+    }
+}
---
+++ Lab10_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab10_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Artist prince = new Artist("Prince");
+            prince.AddToPlaylist("Purple Rain");
+            prince.AddToPlaylist("Little Red Corvette");
+            prince.AddToPlaylist("Kiss");
+            prince.AddToPlaylist("When Doves Cry");
+
+            MediaPlayer player = new MediaPlayer();
+            player.PlayMedia(prince);
+
+            AudioBook howTo = new AudioBook("How to Win Friends and Influence People", "Dale Carnegie");
+            player.PlayMedia(howTo);
+
+            MovieSoundTrack starWars = new MovieSoundTrack("Star Wars");
+            starWars.SelectTrack("Opening Credits Theme");
+            player.PlayMedia(starWars);
+        }
+    }
+}
